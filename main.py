3508e51a2c332ee8cfefa1985673ea5d665735be
IRONmanAbhi <ironmanabhinav1@gmail.com>
2023-01-30 16:32:54 +0530

    minor error fixed

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def get_urls(json_dict):
         if i["is_answered"]:
             url_list.append(i["link"])
         count += 1
-        if count == 3:
+        if count == 4:
             break
 
     for i in url_list:
@@ -34,14 +34,19 @@ def get_urls(json_dict):
 
 if __name__ == "__main__":
     file_name = sys.argv[1]
+    print(file_name)
     subprocess.run(["python", file_name])
-    op, err = execute_return("python {}".format(file_name))
+    op, err = execute_return("python {0}".format(file_name))
     error_message = err.decode("utf-8").strip().split("\r\n")[-1]
     if error_message:
         filter_err = error_message.split(":")
         json1 = make_request(filter_err[0])
         json2 = make_request(filter_err[1])
+        json3 = make_request(filter_err[2])
+        json4 = make_request(filter_err[3])
         json = make_request(error_message)
+        get_urls(json4)
+        get_urls(json3)
         get_urls(json1)
         get_urls(json2)
         get_urls(json)
